b8c25b0a4259c34464ba3f8a3ffb02bbe1d3ecb5
masashi.g <masashi.g@gmail.com>
2020-01-01 17:20:08 +0900

    kvs example

---
+++ kvs_keys.js
@@ -0,0 +1,11 @@
+
+// AWS reagion
+const AWS_REGION = 'us-west-2';
+
+// DescribeSignalingChannel API can also be used to get the ARN from a channel name.
+const AWS_CHANNEL_ARN = 'arn:aws:kinesisvideo:us-west-2:xxxxx:channel/xxxxx/xxxxxxx';
+
+// AWS Credentials
+const AWS_ACCESS_KEY_ID = 'xxxxxxxxxx';
+const AWS_SECRET_ACCESS_KEY = 'xxxx/xxxx';
+
